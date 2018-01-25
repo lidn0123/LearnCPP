95bd91f78081e0fda4f094f1460194ac6dac267e
LI <lidn0123@gmail.com>
2018-01-25 17:42:33 +0800

    FirstTest

---
+++ myfirst.cpp
@@ -0,0 +1,16 @@
+//
+// Created by lidn0 on 18/1/25.
+//
+
+#include <iostream>
+
+int main() {
+    using namespace std;
+    cout << "Come Up and C++ me some time";
+    cout << endl;
+    cout << "You won't regret it!" << endl;
+
+    cin.get();
+    return 0;
+    
+}
